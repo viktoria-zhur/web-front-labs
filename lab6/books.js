3bce2dde2d881bf3f82d3498cb2838eb50d6b196
Viktoria Zhur <papilata05@gmail.com>
2025-06-13 23:54:53 +0700

    books

---
+++ lab6/books.js
@@ -0,0 +1,92 @@
+const books = [
+    {
+        num: 1,
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 187,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        num: 2,
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 480,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        num: 3,
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 549,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        num: 4,
+        author: 'Рэй Брэдбери',
+        name: '451 градус по фаренгейту',
+        publicationYear: 2024,
+        price: 289,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        num: 5,
+        author: 'Антуан де Сент-Экзюпери',
+        name: 'Маленький принц',
+        publicationYear: 2023,
+        price: 149,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        num: 6,
+        author: 'Михаил Шолохов',
+        name: 'Тихий Дон',
+        publicationYear: 2021,
+        price: 408,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        num: 7,
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 2025,
+        price: 129,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        num: 8,
+        author: 'Джейн Остин',
+        name: 'Гордость и предубеждение',
+        publicationYear: 2024,
+        price: 109,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        num: 9,
+        author: 'Виктор Мари Гюго',
+        name: 'Отверженные',
+        publicationYear: 2022,
+        price: 329,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        num: 10,
+        author: 'Эрих Мария Ремарк',
+        name: 'Жизнь взаймы',
+        publicationYear: 2023,
+        price: 255,
+        bestseller: false,
+        inStock: true
+    }
+];
